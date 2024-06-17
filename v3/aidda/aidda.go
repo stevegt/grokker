a50fd75c038bf6653597a028aab4d6cb32918bf3
Steve Traugott <stevegt@t7a.org>
2024-06-17 16:45:31 -0700

    Streamline test specs, rearrange imports/functions, refine timing
    Remove stdout/stderr, update file sections, amend test times
    
    summary of diff --git  a/x/x3/.aidda/prompt b/x/x3/.aidda/prompt
    
    - Remove test case creation lines specifying "stdout" and "stderr" outputs
    - Add "aidda_test.go" to the "In" section
    - Add "aidda.go" and "run.go" to the "Out" section
    - Update instruction to move "Run*" functions to "run.go"
    - Amend "TestRunTee" execution time from 0.11s to 0.10s
    - Amend "TestRun" execution time from 0.01s to 0.00s
    - Update overall execution time from 0.138s to 0.136s
    
    Remove unused imports and obsolete functions in aidda.go.
    
    summary of diff --git  a/x/x3/aidda.go b/x/x3/aidda.go
    
    - Remove import of io/ioutil
    - Remove import of os/exec
    - Remove import of github.com/google/shlex
    - Remove the RunTee function
    - Remove the Run function
    - Remove the RunInteractive function
    
    Add x/x3/run.go with RunTee, Run, and RunInteractive functions.
    
    summary of diff --git  a/x/x3/run.go b/x/x3/run.go
    
    - Add a new file x/x3/run.go
    - Implement RunTee function to run a shell command with stdout and stderr tee'd to the terminal
    - Implement Run function to run a shell command with stdin, stdout, stderr, and return code handling
    - Implement RunInteractive function to run a shell command with stdio connected to the terminal

--- v3/aidda/aidda.go
+++ v3/aidda/aidda.go
@@ -4,9 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -15,148 +13,11 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/emersion/go-message/mail"
 	"github.com/fsnotify/fsnotify"
-	"github.com/google/shlex"
 	"github.com/stevegt/envi"
 	. "github.com/stevegt/goadapt"
 	"github.com/stevegt/grokker/v3/core"
 	"github.com/stevegt/grokker/v3/util"
 )
-
-// RunTee runs a command in the shell, with stdout and stderr tee'd to the terminal
-func RunTee(command string) (stdout, stderr []byte, rc int, err error) {
-	defer Return(&err)
-	// shlex the command to get the command and args
-	parts, err := shlex.Split(command)
-	Ck(err)
-	var args []string
-	var cmd string
-	if len(parts) > 1 {
-		cmd = parts[0]
-		args = parts[1:]
-	} else {
-		cmd = parts[0]
-	}
-	// create the command
-	cobj := exec.Command(cmd, args...)
-	// connect stdin to the terminal
-	cobj.Stdin = os.Stdin
-
-	// create a tee for stdout
-	stdoutPipe, err := cobj.StdoutPipe()
-	Ck(err)
-	stdoutTee := io.TeeReader(stdoutPipe, os.Stdout)
-	// create a tee for stderr
-	stderrPipe, err := cobj.StderrPipe()
-	Ck(err)
-	stderrTee := io.TeeReader(stderrPipe, os.Stderr)
-	// read the stdout in a goroutine
-	go func() {
-		stdout, err = ioutil.ReadAll(stdoutTee)
-		Ck(err)
-	}()
-
-	// read the stderr in a goroutine
-	go func() {
-		stderr, err = ioutil.ReadAll(stderrTee)
-		Ck(err)
-	}()
-	// wait for goroutines to get started
-	// XXX use a waitgroup instead
-	time.Sleep(100 * time.Millisecond)
-
-	// start the command
-	err = cobj.Start()
-	Ck(err)
-	// wait for the command to finish
-	err = cobj.Wait()
-	Ck(err)
-	// get the return code
-	rc = cobj.ProcessState.ExitCode()
-	return
-}
-
-// Run runs a command in the shell, returning stdout, stderr, and rc
-func Run(command string, stdin []byte) (stdout, stderr []byte, rc int, err error) {
-	defer Return(&err)
-	// shlex the command to get the command and args
-	parts, err := shlex.Split(command)
-	Ck(err)
-	var args []string
-	var cmd string
-	if len(parts) > 1 {
-		cmd = parts[0]
-		args = parts[1:]
-	} else {
-		cmd = parts[0]
-	}
-	// create the command
-	cobj := exec.Command(cmd, args...)
-	// create a pipe for stdin
-	stdinPipe, err := cobj.StdinPipe()
-	Ck(err)
-	// create a pipe for stdout
-	stdoutPipe, err := cobj.StdoutPipe()
-	Ck(err)
-	// create a pipe for stderr
-	stderrPipe, err := cobj.StderrPipe()
-	Ck(err)
-	// start the command
-	err = cobj.Start()
-	Ck(err)
-	// pipe stdin to the command in a goroutine
-	go func() {
-		_, err = stdinPipe.Write(stdin)
-		Ck(err)
-		stdinPipe.Close()
-	}()
-	// read the stdout in a goroutine
-	go func() {
-		stdout, err = ioutil.ReadAll(stdoutPipe)
-		Ck(err)
-	}()
-	// read the stderr in a goroutine
-	go func() {
-		stderr, err = ioutil.ReadAll(stderrPipe)
-		Ck(err)
-	}()
-	// wait for the command to finish
-	err = cobj.Wait()
-	Ck(err)
-	// get the return code
-	rc = cobj.ProcessState.ExitCode()
-	return
-}
-
-// RunInteractive runs a command in the shell, with stdio connected to the terminal
-func RunInteractive(command string) (rc int, err error) {
-	defer Return(&err)
-	// shlex the command to get the command and args
-	parts, err := shlex.Split(command)
-	Ck(err)
-	var args []string
-	var cmd string
-	if len(parts) > 1 {
-		cmd = parts[0]
-		args = parts[1:]
-	} else {
-		cmd = parts[0]
-	}
-	// create the command
-	cobj := exec.Command(cmd, args...)
-	// connect the stdio to the terminal
-	cobj.Stdin = os.Stdin
-	cobj.Stdout = os.Stdout
-	cobj.Stderr = os.Stderr
-	// start the command
-	err = cobj.Start()
-	Ck(err)
-	// wait for the command to finish
-	err = cobj.Wait()
-	Ck(err)
-	// get the return code
-	rc = cobj.ProcessState.ExitCode()
-	return
-}
 
 /*
 - while true
