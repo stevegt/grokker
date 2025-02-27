5ea8494bd8fba87d283f6486e452a4bc4a902397
Steve Traugott <stevegt@t7a.org>
2025-02-27 12:45:03 -0800

    bump version to v3.0.25

--- v3/core/grokker.go
+++ v3/core/grokker.go
@@ -51,7 +51,7 @@ import (
 const (
 	// See the "Semantic Versioning" section of the README for
 	// information on API and db stability and versioning.
-	Version = "3.0.24"
+	Version = "3.0.25"
 )
 
 type Grokker struct {
